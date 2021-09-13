2d960df0c0951708ba5b4f184a159b94bb993922
CLAY <82664613+WhoAmI-CLAY@users.noreply.github.com>
2021-09-13 15:08:22 +0900

    나의 쇼핑 리스트

---
+++ ToDoList/index.js
@@ -0,0 +1,60 @@
+const ToDoListForm = document.querySelector(".ToDoListForm")
+const ToDoListinput = document.querySelector(".ToDoListForm input")
+const ToDoList = document.querySelector(".ToDoList")
+
+const TODOLIST_KEY = 'todos' 
+let ToDos = [] //생성한 ToDolist를 저장
+
+function saveToDos() {
+    localStorage.setItem(TODOLIST_KEY, JSON.stringify(ToDos))
+}
+
+function removeToDoList(event){
+    let li = event.target.parentElement;
+    li.remove();
+
+    ToDos = []
+    for(let i = 0; i < ToDoList.childElementCount; i++) {
+        let value = $(ToDoList).find(`li:nth-child(${i + 1})`).find('span').text();
+        ToDos.push(value)
+    }
+    saveToDos()
+}
+
+function paintToDo(newToDo) {   
+    console.log(newToDo)
+    let li = document.createElement('li')
+    let span = document.createElement('span')
+    let button = document.createElement("button")
+    span.innerText = newToDo;
+    button.innerText = "X";
+    li.appendChild(span)
+    li.appendChild(button)
+    ToDoList.appendChild(li)
+    button.addEventListener('click', removeToDoList)
+}
+
+function ToDoListFormEvent(event){
+    event.preventDefault();
+    const value = ToDoListinput.value
+    ToDoListinput.value = "";
+    if(value.length != 0) {
+        paintToDo(value)
+        ToDos.push(value)
+        saveToDos();
+    }
+}
+
+ToDoListForm.addEventListener("submit", ToDoListFormEvent)
+
+
+// 라면 감자 고구마
+
+if(localStorage.getItem(TODOLIST_KEY) !== null){
+    let getToDoListItem = JSON.parse(localStorage.getItem(TODOLIST_KEY))
+    ToDos = getToDoListItem;
+    console.log(ToDos)
+    for(let i = 0; i < getToDoListItem.length; i++){
+        paintToDo(getToDoListItem[i])
+    }
+}
